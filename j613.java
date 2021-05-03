bb4fd51fa1220442667b64945937cf16231c67d5
uno-km <82789634+uno-km@users.noreply.github.com>
2021-05-03 17:42:35 +0900

    Update j613.java
    Edit class Person

--- j613.java
+++ j613.java
@@ -2,28 +2,35 @@ package Struct;
 
 import java.util.Scanner;
 
-public class j613 {
+public class j613ver3 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String name = sc.next();
+		String school = sc.next();
+		int grade = sc.nextInt();
+		sc.close();
+		Person p = new Person(name, school, grade);
+		p.print_Inform();
+	}
+
+}
+
+class Person {
 	private String name;
 	private String school;
 	private int grade;
 
-	j613() {
-		Scanner sc = new Scanner(System.in);
-		this.name = sc.next();
-		this.school = sc.next();
-		this.grade = sc.nextInt();
-		sc.close();
+	public Person(String name, String school, int grade) {
+		this.name = name;
+		this.school = school;
+		this.grade = grade;
 	}
 
 	public void print_Inform() {
-		System.out.println("Name : " + name);
-		System.out.println("School : " + school);
-		System.out.println("Grade : " + grade);
-	}
-
-	public static void main(String[] args) {
-		j613 m = new j613();
-		m.print_Inform();
+		System.out.printf("Name : %s\n", name);
+		System.out.printf("School : %s\n", school);
+		System.out.printf("grade : %d\n", grade);
 	}
 
 }
