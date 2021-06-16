72add0db5224f2d6b5a7c003fb6c2da1ff97cb01
uno-km <82789634+uno-km@users.noreply.github.com>
2021-06-16 15:42:34 +0900

    Update j551.java

--- j551.java
+++ j551.java
@@ -1,10 +1,24 @@
 package f_RepeatedControl3;
 
+import java.util.Scanner;
+
 public class j551 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		for (int i = 0; i < a; i++) {
+			for (int k = 0; k < i; k++) {
+				System.out.print(" ");
+			}
+			for (int j = i; j < a; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 
+		sc.close();
 	}
 
 }
