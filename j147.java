c44be3a4517d183856ef4b7a1bf7f61b80a4ac6b
uno-km <82789634+uno-km@users.noreply.github.com>
2021-06-16 15:41:03 +0900

    Update j147.java

--- j147.java
+++ j147.java
@@ -1,10 +1,24 @@
 package f_RepeatedControl3;
 
+import java.util.Scanner;
+
 public class j147 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int c = sc.nextInt(), cnt = 1;
+		for (int i = 0; i < c; i++) {
+			for (int k = 0; k <= i - 1; k++) {
+				System.out.print("  ");
+			}
+			for (int j = 0; j < c - i; j++) {
+				System.out.print(cnt++ + " ");
+			}
+			System.out.println();
 
+		}
+		sc.close();
 	}
 
 }
