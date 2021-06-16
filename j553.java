41a8f0e3102abcb7cbe99629806a2b675ab262db
uno-km <82789634+uno-km@users.noreply.github.com>
2021-06-16 15:47:15 +0900

    Update j553.java

--- j553.java
+++ j553.java
@@ -1,5 +1,20 @@
 package f_RepeatedControl3;
 
-public class j553 {
+import java.util.Scanner;
 
+public class j553 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt(), sum = 0;
+		char b = (int) 65;
+//      System.out.println(b);
+		for (int i = 0; i < a; i++) {
+			for (int j = 0; j < a - i; j++) {
+//              b += 1;
+				System.out.printf("%c", b++);
+			}
+			System.out.println();
+		}
+		sc.close();
+	}
 }
